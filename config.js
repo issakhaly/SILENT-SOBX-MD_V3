0075c53ac9fb3d93692826294fcf65397300b207
issakhaly <157059290+issakhaly@users.noreply.github.com>
2025-02-09 23:51:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -26,12 +26,12 @@ module.exports = {
     CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
     AUTO_REACT: process.env.AUTO_REACT || "false",
     HEART_REACT: process.env.HEART_REACT || "false",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
     READ_CMD: process.env.READ_CMD || "true",
     BOT_NAME: process.env.BOT_NAME || "LIN KUEI",
     STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү sιℓεηт-sσвx-м∂`",
     STATUS_REACT: process.env.STATUS_REACT || "false",
-    ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
+    ANTI_VV: process.env.ANTI_VV || "false",// true for anti once view 
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'log' if you want to resend deleted message in ib chat 
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
